92ddba5c76b04ec07c9c9f4a9d14a70e3e452678
22zkeyan <22ZKeyan@heckgrammar.co.uk>
2026-05-26 20:01:11 +0100

    attempted problem 5 in the main

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -42,7 +42,7 @@ namespace ProjectEuler
         // Largest Prime Factor
         static void Problem3()
         {
-            int[] smallPrimes = { 2, 3, 5, 7 }; // single digit prime factors
+            int[] smallPrimes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,  }; // single digit prime factors
             long number = 600851475143;
             long[] factors = new long[number / 2];
             for (int i = 0; i < factors.Length; i++)
@@ -58,9 +58,36 @@ namespace ProjectEuler
                 }
             }
         }
+
+        //// Problem 3: Check if prime function
+        //static bool CheckPrime(long num)
+        //{
+        //    if (num == 0 || num == 1 || num == 2)
+        //    {
+        //        return false;
+        //    }
+        //    else
+        //    {
+        //        long sqrt = (long)Math.Floor(Math.Sqrt(num)); // finds the square root and then rounds it down
+
+        //    }
+        //}
         static void Main(string[] args)
         {
-            Problem2();
+            for (int i = 0; i < 1000; i++)
+            {
+                int product = 7 * i;
+                if (product % 20 == 0 && product % 9 == 0 && product % 6 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(product);
+
+            }
         }
     }
 }
